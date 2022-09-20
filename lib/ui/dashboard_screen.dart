de8bc62e310e03148fb69af653a4a01c24822510
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2022-09-20 15:30:38 +0530

    Added Profile button in appbar in dashboard

--- lib/ui/dashboard_screen.dart
+++ lib/ui/dashboard_screen.dart
@@ -9,8 +9,20 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const NavDrawer(),
-      appBar: AppBar(title: const Text("Dashboard")),
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 1, 20, 1),
+            width: 58,
+            child: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://4.bp.blogspot.com/-Jx21kNqFSTU/UXemtqPhZCI/AAAAAAAAh74/BMGSzpU6F48/s1600/funny-cat-pictures-047-001.jpg"),
+              backgroundColor: Colors.red,
+            ),
+          ),
+        ],
+      ),
     );
   }
-
 }

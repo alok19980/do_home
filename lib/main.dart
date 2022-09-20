ac1a58e93e737104faeed1917a9f80443536f341
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2022-09-20 15:00:02 +0530

    connected to firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:do_home/ui/dashboard_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,13 +17,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-      title: 'DoHome',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-      ),
-      home: const DashboardScreen()
-    );
+      debugShowCheckedModeBanner: false,
+        title: 'DoHome',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const DashboardScreen());
   }
 }

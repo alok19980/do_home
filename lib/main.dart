b736b64404fd92ad2bfe64966669f47b0f9bac59
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2022-09-20 12:45:15 +0530

    Updated name for app

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DoHome',
       theme: ThemeData(
         
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'DoHome Dashboard'),
     );
   }
 }

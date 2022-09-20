1506f0cf87b762295238bbfb99ea65ba98468878
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2022-09-20 15:11:36 +0530

    Added Basic Side Navigation Drawer

--- lib/ui/dashboard_screen.dart
+++ lib/ui/dashboard_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/nav_drawer.dart';
+
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const NavDrawer(),
       appBar: AppBar(title: const Text("Dashboard")),
     );
   }
